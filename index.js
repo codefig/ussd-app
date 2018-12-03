f9dc986fb11b969e1d022ef70fe664b8938d8ed9
Moshood Alaran <geekyshood@gmail.com>
2018-12-03 04:09:15 +0100

    initial repo configs

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser');
+const db = require('mongoose');
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', function(req, res, next){
+    console.log("The index function");
+    res.send("Welcome to the homepage");
+})
+
+
+app.listen(PORT, function(){
+    console.log(`Application served on : ${PORT}`)
+})
