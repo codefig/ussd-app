e7b7beafff81a5581fdf4d8b0e5b91cc1cd332fe
Moshood Alaran <geekyshood@gmail.com>
2018-12-05 18:17:50 +0100

    effected the changes to number error

--- server.js
+++ server.js
@@ -8,33 +8,34 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the Emergency Operation Unit, how can we help you ?')
 })
 
-app.post('*', (req, res) => {
+app.post('/report', (req, res) => {
   let {sessionId, serviceCode, phoneNumber, text} = req.body
   let name = "";
   let address = "";
   let details = "";
   let textValue = text.split('*').length;
+  console.log(req.body);
 
   let message = "";
   if(text == ''){
-    message = `CON Welcome ${phoneNumber} to EOU, how can we help you today ${textValue} ?`
-    console.log(textValue);
+    message = `CON Welcome to EOU, how can we help you today  ?`
+    console.log(req.body);
   }
-  else if(text == 1){
+  else if(textValue == 1){
      message = "CON Please kindly tell us your name . "
      name = text;
   }
 
-  else if(text == 2){
+  else if(textValue == 2){
     message = "CON Please Kindly tell us the address of the emergency "
     address = text;
   }
 
-  else if(text == 3){
+  else if(textValue == 3){
     message = "CON please describe briefly the situation on ground."
     details = text;
   }
