db261323aba4e0566ba7a81ca06c579acaf54dba
Moshood Alaran <geekyshood@gmail.com>
2018-12-06 13:12:00 +0100

    testing new record of emergency

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const logger = require('morgan')
-const path  = require('path')
+const path = require('path')
 
 const Emergency = require('./connection/schema')
 
@@ -11,66 +11,66 @@ const port = process.env.PORT || 3030
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(express.static('./static'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
 
 
 app.get('/', (req, res) => {
-  
-  
+
+
   let emergency = {
-    name : "Abass Adekunle", 
-    userLocation : "Sambog kdkd", 
-    distressLocation : "Kano ", 
-    details : "wlskdkflkskdkf"
+    name: "Abass Adekunle",
+    userLocation: "Sambog kdkd",
+    distressLocation: "Kano ",
+    details: "wlskdkflkskdkf"
   }
 
   res.sendFile(path.join(__dirname, './static', 'index.html'));
 })
 
-app.post('/', async function (req, res){
-  let {sessionId, serviceCode, phoneNumber, text} = req.body
+app.post('/', async function (req, res) {
+  let { sessionId, serviceCode, phoneNumber, text } = req.body
   let name = "";
   let userLocation = "";
   let distressLocation = "";
   let details = "";
   let textValue = text.split('*').length;
+  let record = new Emergency({
+    name: "",
+    userLocation: "",
+    distressLocation: "",
+    details: details
+  })
   console.log(req.body);
-  
+
   let message = "";
-  if(text == ''){
+  if (text == '') {
     message = `CON Welcome to Nigerian Anti-Terrorism Unit, Please enter your name to continue?`
     console.log(req.body);
   }
-  else if(textValue == 1){
-    name = text;
+  else if (textValue == 1) {
+    record.name = text;
     message = "CON Please kindly tell us your location."
   }
-  else if(textValue == 2){
-    userLocation = text.split('*')[1];
+  else if (textValue == 2) {
+    record.userLocation = text.split('*')[1];
     message = "CON Please Kindly tell us the address of the distress"
   }
-  
-  else if(textValue == 3){
-    distressLocation = text.split('*')[2];
+
+  else if (textValue == 3) {
+    record.distressLocation = text.split('*')[2];
     message = "CON please describe briefly the situation on ground."
   }
-  else if(textValue == 4){
-    details = text.split('*')[3];
+  else if (textValue == 4) {
+    record.details = text.split('*')[3];
     message = `END Thanks ${name} We would gladly respond to this distress soon`;
   }
-  else{
+  else {
     message = "END Thank you for getting in touch with us . ";
   }
-  
-  let record = new Emergency({
-    name : name, 
-    userLocation : userLocation, 
-    distressLocation : distressLocation, 
-    details : details
-  })
+
   console.log(record);
   res.send(message);
 })
